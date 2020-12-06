1ba830f26ae8feb5987df2005bed51492b542aa5
Ameen <md.ameenullah.h@gmail.com>
2020-12-07 00:40:34 +0530

    random chuck norris joke - final commit

---
+++ Random Chuck Norris Joke- API/app.js
@@ -0,0 +1,61 @@
+const btn = document.querySelector('.btn');
+const content = document.querySelector('.content');
+const img = document.querySelector('.container img')
+
+const URL = 'https://api.chucknorris.io/jokes/random';
+
+btn.addEventListener('click', async () =>{
+    try {
+        const data =  await fetch(URL);
+        const response = await data.json();
+        displayData(response);
+    } catch (error) {
+        console.log(error);
+    }
+    
+})
+
+
+function displayData({value:joke}){
+    img.classList.add('shake-img');
+    content.textContent = joke;
+    const random = Math.random() * 1000;
+    setTimeout(() =>{
+    img.classList.remove('shake-img');
+    },random)
+}
+
+//using fetch
+// btn.addEventListener('click', () =>{
+//     fetch(URL)
+//     .then(data => data.json())
+//     .then(d => displayData(d))
+//     .catch(err => console.log(err));
+// })
+
+
+
+// function getData(url){
+//     return new Promise((resolve,reject) =>{
+
+//         const xhr = new XMLHttpRequest();
+//         xhr.open('GET',url);
+//         xhr.send();
+//         xhr.onreadystatechange = () =>{
+//             if(xhr.readyState !==4) 
+//                 return;
+//             if(xhr.status === 200){
+//                 resolve(xhr.responseText);
+//             }
+//             else{
+//                 reject({
+//                     status:xhr.status,
+//                     text: xhr.statusText,
+//                     state:xhr.readyState,
+//                 });
+//             }
+//         };
+
+//     });
+
+// }
